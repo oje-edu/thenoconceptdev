4834bec90ead5ced3e6b15c68a4791fa97de42ce
oje-edu <thorsten.oje@gfn.education>
2021-07-07 08:29:53 +0200

    further work on babbeln

--- src/Pages/Linkedin.js
+++ src/Pages/Linkedin.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Sidebar from '../Components/Sidebar'
 
 function Linkedin () {
   return (
-    <div>
+    <div className='linkedin'>
       <Header />
+      <div className='linkedin__body'>
+        <Sidebar />
+      </div>
       <h1>In Bearbeitung</h1>
     </div>
   )
