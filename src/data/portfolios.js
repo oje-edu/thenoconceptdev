fae9fa3436b5939a1e0332b76f9c207d8d944e5f
oje-edu <thorsten.oje@gfn.education>
2021-07-11 17:34:03 +0200

    added some links to portfolio

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,4 +1,5 @@
-// import css1 from '../img/portImages/cv_css.png'
+import parallax from '../img/portImages/parallax.jpeg'
+import sass from '../img/portImages/sass.jpeg'
 // import css2 from '../img/portImages/ms.png'
 // import react1 from '../img/portImages/react_styled.png'
 // import img2 from '../img/portImages/maya-4.jpg'
@@ -26,11 +27,20 @@ const portfolios = [
   {
     id: 3,
     category: 'Animation',
-    // image: img3,
-    link1: 'https://shadowdog.oje.is/',
-    link2: '#',
-    title: 'Spiel Animation',
+    image: parallax,
+    link1: 'https://github.com/oje-edu/seriously-7-js-shadowdog/',
+    link2: 'https://shadowdog.oje.is/gameAnimations.html',
+    title: 'JS - Spiel Animationen',
     text: 'Sprites, Parallax und NPC'
+  },
+  {
+    id: 4,
+    category: 'SASS',
+    image: sass,
+    link1: 'https://github.com/oje-edu/seriously-8-sass-boilerplate',
+    link2: 'https://sass.oje.is/',
+    title: 'SASS Boilerplate',
+    text: 'So ziemlich der neueste Shit was SASS angeht.. (2021)'
   }
 ]
 
